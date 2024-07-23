17e376c1759eeb481bdd92278d0c97b1eccf2f3d
Hector Garcia <hmgarcia06@gmail.com>
2024-07-23 11:37:15 -0400

    Added simple nextjs page type project

---
+++ nextjs-pages/next.config.mjs
@@ -0,0 +1,21 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/step/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin if needed
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
+  }
+  
+  export default nextConfig;
---
+++ nextjs-pages/src/pages/api/index.ts
@@ -0,0 +1,11 @@
+import NextCors from 'nextjs-cors';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    await NextCors(req, res, {
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      origin: '*',
+      optionsSuccessStatus: 200,
+    });
+    res.status(200).json({ message: 'Hello Weave!', status: 200 });
+};
