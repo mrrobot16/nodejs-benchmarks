865fabe2516999659b96478378a155c36907f661
Hector Garcia <hmgarcia06@gmail.com>
2024-07-23 19:49:56 -0400

    Added mongodb logic for nextjs-pages

--- nextjs-pages/src/pages/api/index.ts
+++ nextjs-pages/src/pages/api/index.ts
@@ -1,5 +1,10 @@
 import NextCors from 'nextjs-cors';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
+
+
+const url = "mongodb+srv://dylan:43VFMVJVJUFAII9g@cluster0.8phbhhb.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(url)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     await NextCors(req, res, {
@@ -7,5 +12,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       origin: '*',
       optionsSuccessStatus: 200,
     });
-    res.status(200).json({ status: 200 });
+
+    try {
+		await client.connect();
+		const db = client.db('weave-dev-db');
+		const workflows = await db.collection('workflows').find({ user_id: 'hmgaar@gmail.com' }).toArray();
+		const data = {
+			workflows
+		};
+		res.status(200).json({ status: 200, data });
+	  } catch (error) {
+		console.error('GET_WORKFLOW_ERROR_MESSAGE', error);
+	  } finally {
+		await client.close();
+	  }
+    
 };
