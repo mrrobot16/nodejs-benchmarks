d4288653713dafe0e067186cd034abd7424cf7b2
Hector Garcia <hmgarcia06@gmail.com>
2024-07-23 19:49:35 -0400

    Added mongodb logic for nextjs-app

--- nextjs-app/src/app/api/route.ts
+++ nextjs-app/src/app/api/route.ts
@@ -1,7 +1,26 @@
 import { NextResponse } from 'next/server'
 
-const handler = () => {
-    return NextResponse.json({ status: 200 })
+import { MongoClient } from 'mongodb'
+
+const url = "mongodb+srv://dylan:43VFMVJVJUFAII9g@cluster0.8phbhhb.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(url)
+
+const handler = async () => {
+
+    try {
+		await client.connect();
+		const db = client.db('weave-dev-db');
+		const workflows = await db.collection('workflows').find({ user_id: 'hmgaar@gmail.com' }).toArray();
+		const data = {
+			workflows
+		};
+		return NextResponse.json({ status: 200, data })
+	  } catch (error) {
+		console.error('GET_WORKFLOW_ERROR_MESSAGE', error);
+	  } finally {
+		await client.close();
+	  }
+
 }
 
 export { handler as GET }
