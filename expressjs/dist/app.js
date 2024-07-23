a617a5d94af34885033cf344be7f39b2f0ff433a
Hector Garcia <hmgarcia06@gmail.com>
2024-07-23 19:46:39 -0400

    Added mongdb query logic to measure response time in expressjs

--- expressjs/dist/app.js
+++ expressjs/dist/app.js
@@ -6,16 +6,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const mongodb_1 = require("mongodb");
 const middlewares_1 = require("./middlewares");
+const url = "mongodb+srv://dylan:43VFMVJVJUFAII9g@cluster0.8phbhhb.mongodb.net/?retryWrites=true&w=majority";
+const client = new mongodb_1.MongoClient(url);
 exports.app = (0, express_1.default)();
 exports.app.use(middlewares_1.CORS);
 // Middleware to log requests
 exports.app.use((0, morgan_1.default)('combined'));
 // Middleware to parse JSON bodies
 exports.app.use(express_1.default.json());
-exports.app.get('/', (req, res) => {
-    res.send({
-        status: 200
-    });
+exports.app.get('/', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db('weave-dev-db');
+        const workflows = await db.collection('workflows').find({ user_id: 'hmgaar@gmail.com' }).toArray();
+        const data = {
+            workflows
+        };
+        res.send({
+            status: 200,
+            data
+        });
+    }
+    catch (error) {
+        console.error('GET_WORKFLOW_ERROR_MESSAGE', error);
+    }
+    finally {
+        await client.close();
+    }
 });
 //# sourceMappingURL=app.js.map
--- expressjs/src/app.ts
+++ expressjs/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
+import { MongoClient } from 'mongodb'
 
 import { CORS } from '@/middlewares'
 
+
+const url = "mongodb+srv://dylan:43VFMVJVJUFAII9g@cluster0.8phbhhb.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(url)
 export const app = express()
 
 app.use(CORS);
@@ -13,8 +17,23 @@ app.use(morgan('combined'))
 // Middleware to parse JSON bodies
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send({
-	status: 200
-  })
+app.get('/', async (req, res) => {
+    try {
+		await client.connect();
+		const db = client.db('weave-dev-db');
+		const workflows = await db.collection('workflows').find({ user_id: 'hmgaar@gmail.com' }).toArray();
+		const data = {
+			workflows
+		};
+
+		res.send({
+			status: 200,
+			data
+		  })
+	  } catch (error) {
+		console.error('GET_WORKFLOW_ERROR_MESSAGE', error);
+	  } finally {
+		await client.close();
+	  }
+
 })
